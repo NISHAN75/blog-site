3b71390f6df435faa425dfdb32cbb7bcabe9d9d7
NISHAN75 <nishandas880@gmail.com>
2024-04-09 13:23:58 +0600

    add

--- slider/assets/js/main.js
+++ slider/assets/js/main.js
@@ -1,8 +1,32 @@
 (function ($) {
    $(document).ready(function () {
-
-
-       
+      let swiper = new Swiper(".bl-slider-wrapper", {
+         slidesPerView: 5,
+         spaceBetween: 10,
+         navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          breakpoints: {
+            // when window width is >= 320px
+            1200: {
+              slidesPerView: 5,
+              spaceBetween: 10
+            },
+            992: {
+              slidesPerView: 3,
+              spaceBetween: 10
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 10
+            },
+            300: {
+              slidesPerView: 1,
+              spaceBetween: 10
+            },
+          }
+       });
    });
 })(jQuery);
 
